9c95adbd0170102e55cbea8fd657482557c1ed9a
Arjun Dandagi <dandagi.arjun95@gmail.com>
2021-08-24 20:39:29 +0530

    Update dirtouch@2.rb

--- Formula/dirtouch@2.rb
+++ Formula/dirtouch@2.rb
@@ -1,12 +1,8 @@
 class DirtouchAT2 < Formula
-  desc "dirtouch = mkdir -p + touch "
-  homepage ""
+  desc "dirtouch = mkdir -p + touch"
   url "https://github.com/ArjunDandagi/dirtouch/archive/refs/tags/latest.zip"
   version "2.0.0"
-  sha256 ""
-  license ""
-
-  # depends_on "cmake" => :build
+  sha256 "1cfe6659f1fe58a771c51e0d716c6ce776beed11ed0fd86ed813f012f00347b4"
 
   def install
         bin.install "dirtouch"
