f7f4e1f7d4141eb831766aeca7a0606e1aef1386
Arjun Dandagi <dandagi.arjun95@gmail.com>
2021-08-26 13:38:09 +0530

    Update dirtouch@2.rb

--- Formula/dirtouch@2.rb
+++ Formula/dirtouch@2.rb
@@ -2,7 +2,7 @@ class DirtouchAT2 < Formula
   desc "dirtouch = mkdir -p + touch"
   url "https://github.com/ArjunDandagi/dirtouch/archive/refs/tags/latest.zip"
   version "2.0.0"
-  sha256 "1cfe6659f1fe58a771c51e0d716c6ce776beed11ed0fd86ed813f012f00347b4"
+  sha256 "8e22c0a578a7f4606cf53e8367d759a35ab03eba7e46e963f7a07019c84d0d7f"
 
   def install
         bin.install "dirtouch"
